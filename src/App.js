1ebb73170e17d988d35e074d3276d3555c2871f9
Mohamed Asir <asir.abm@gmail.com>
2022-12-23 23:37:00 +0530

    finished styling part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 //import { render } from "@testing-library/react";
 import React from "react";
 //import { ReactDOM } from "react";
+import CurrencyConvertor from "./componets/CurrencyConvertor";
 import './app.css'
 
 function App(){
   return(
         <div>
-            <h1>Hello from App</h1>
+            <h1>Currency Convertor</h1>
+            <CurrencyConvertor/>
+              <div className="eq">=</div>
+            <CurrencyConvertor/>
         </div>
     )
 }
---
+++ src/componets/CurrencyConvertor.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CurrencyConvertor() {
+  return (
+    <div>
+      <input className='number' type='number'/>
+    <select>
+      <option value='Hi'>Hi</option>
+    </select>
+    </div>
+  )
+}
+
+export default CurrencyConvertor
