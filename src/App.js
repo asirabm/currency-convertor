3cb5ef112684035bf4859f9638d20693b5a041b7
Mohamed Asir <asir.abm@gmail.com>
2022-12-23 10:51:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import { render } from "@testing-library/react";
+import React from "react";
+//import { ReactDOM } from "react";
+import './app.css'
+
+function App(){
+  return(
+        <div>
+            <h1>Hello from App</h1>
+        </div>
+    )
+}
+export default App;
+
